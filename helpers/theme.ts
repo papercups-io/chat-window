f0278255f76d4ac0d4cf7ee2e10f3dedeaa7e1dc
reichert621 <reichertjalex@gmail.com>
2020-11-15 22:03:57 -0500

    Move ponyfills to theme definition

--- helpers/theme.ts
+++ helpers/theme.ts
@@ -1,4 +1,5 @@
 import tinycolor from 'tinycolor2';
+import cssVars from 'css-vars-ponyfill';
 
 type ThemeSettings = {primary?: string};
 
@@ -10,6 +11,31 @@ export const getThemeConfig = (settings: ThemeSettings) => {
     light: base.lighten().toString(),
     dark: base.darken().toString(),
   };
+
+  cssVars({
+    onlyLegacy: false,
+    preserveVars: true,
+    watch: true,
+    variables: {
+      '--theme-ui-colors-text': '#141414',
+      '--theme-ui-colors-background': '#fff',
+      '--theme-ui-colors-primary': overrides.primary,
+      '--theme-ui-colors-darker': overrides.dark,
+      '--theme-ui-colors-lighter': overrides.light,
+      '--theme-ui-colors-secondary': '#722ed1',
+      '--theme-ui-colors-green': '#52c41a',
+      '--theme-ui-colors-muted': '#f0f0f0',
+      '--theme-ui-colors-gray': 'rgba(0, 0, 0, 0.45)',
+      '--theme-ui-colors-input': 'rgba(0, 0, 0, 0.65)',
+      '--theme-ui-colors-offset': 'rgba(255, 255, 255, 0.8)',
+    },
+    onBeforeSend: console.log,
+    onError: console.log,
+    onWarning: console.log,
+    onSuccess: console.log,
+    onComplete: console.log,
+    onFinally: console.log,
+  });
 
   return {
     useBodyStyles: false,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,4 @@
-import cssVars from 'css-vars-ponyfill';
 import '../styles/global.css';
-
-cssVars({
-  onlyLegacy: false,
-  preserveVars: true,
-  watch: true,
-});
 
 export default function App({Component, pageProps}) {
   return <Component {...pageProps} />;
