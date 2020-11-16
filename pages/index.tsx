d8dcbcba2c5910d9e46bd0c38f84b3d64ed07800
reichert621 <reichertjalex@gmail.com>
2020-11-15 22:53:22 -0500

    Try without variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,21 +22,6 @@ export default function Home() {
                 onlyLegacy: false,
                 preserveVars: true,
                 watch: true,
-                variables: {
-                  '--theme-ui-colors-text': '#141414',
-                  '--theme-ui-colors-background': '#fff',
-                  '--theme-ui-colors-primary': '#1890ff',
-                  '--theme-ui-colors-darker': '#1890ff',
-                  '--theme-ui-colors-lighter': '#1890ff',
-                  '--theme-ui-colors-secondary': '#722ed1',
-                  '--theme-ui-colors-green': '#52c41a',
-                  '--theme-ui-colors-muted': '#f0f0f0',
-                  '--theme-ui-colors-gray': 'rgba(0, 0, 0, 0.45)',
-                  '--theme-ui-colors-input': 'rgba(0, 0, 0, 0.65)',
-                  '--theme-ui-colors-offset': 'rgba(255, 255, 255, 0.8)',
-                  '--antd-wave-shadow-color': '#1890ff',
-                  '--scroll-bar': '0',
-                },
               })
             `,
           }}
